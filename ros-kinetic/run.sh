a8d3fdf8554471bc700dfb1dd4c40448066f9290
glc12125 <Liangchuan Gu>
2018-11-01 23:31:21 +0000

    merge

--- ros-kinetic/run.sh
+++ ros-kinetic/run.sh
@@ -14,7 +14,7 @@ set -e
 
 local_dev=$HOME/Development/rosindigo_dev
 if [ ! -d $local_dev ]; then
-	mkdir $local_dev
+	mkdir -p $local_dev
 fi
 
 docker volume create --driver local --opt type=none --opt device=$local_dev --opt o=bind rosindigo_dev
