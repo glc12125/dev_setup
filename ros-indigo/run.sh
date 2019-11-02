62a65fd85fde2e6a5c2fc14806bb0629733ace37
glc12125 <Liangchuan Gu>
2019-11-02 22:29:35 +0000

    use Development as working space

--- ros-indigo/run.sh
+++ ros-indigo/run.sh
@@ -12,22 +12,22 @@ popd > /dev/null
 
 set -e
 
-local_dev=$HOME/Development/rosindigo_dev
+local_dev=$HOME/Development/
 if [ ! -d $local_dev ]; then
 	mkdir -p $local_dev
 fi
 
-docker volume create --driver local --opt type=none --opt device=$local_dev --opt o=bind rosindigo_dev
+#docker volume create --driver local --opt type=none --opt device=$local_dev --opt o=bind rosindigo_dev
 
 xhost + # Allow any connections to X server
 # Run the container with shared X11
 docker run \
   --privileged \
-  --net=host \
   -e SHELL \
   -e DISPLAY \
   -e DOCKER=1 \
-  -v rosindigo_dev:"/home/developer/Development:rw" \
+  -w /Development/ \
+  -v /home/liangchuan/Development:/Development \
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   -v /dev/bus/usb:/dev/bus/usb \
   -v /dev/input:/dev/input \
--- scripts/tensorflow_run.sh
+++ scripts/tensorflow_run.sh
@@ -1,11 +1,16 @@
+# enable x server on host for open access
+# -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"
 xhost +
+# X Error: BadAccess (attempt to access private resource denied) : QT_X11_NO_MITSHM=1
 docker run \
     --runtime=nvidia \
+    --env QT_X11_NO_MITSHM=1 \
     --privileged \
     -e SHELL \
     -e DISPLAY \
     -e DOCKER=1 \
     -v /dev/video0:/dev/video0 \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -it \
     -p 8888:8888 \
     -w /Development/ \
